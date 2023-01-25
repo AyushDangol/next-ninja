9242d3c38857b856ef8f5aa4e399d815e970b4e0
Ayush <ayush.dangol@infodevelopers.com.np>
2023-01-25 14:17:05 +0545

    initail commit

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <h1>Aboutpage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At dolorum
+        repellendus sapiente asperiores ab fuga earum veniam quae. Saepe
+        recusandae dolor error vel totam aliquam tempora tenetur commodi modi
+        repellat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+        inventore fugiat ullam id iusto iste eaque deleniti exercitationem,
+        dolorem itaque, ducimus repudiandae consequatur dolor commodi aspernatur
+        enim tenetur provident quia.
+      </p>
+    </>
+  );
+};
+
+export default About;
